altusnets/external-libnetfilter_conntrack

add KERNEL_DIR option to autogen.sh for all new libraries (Krzysztof Oledzki)

autogen.sh
@@ -7,7 +7,7 @@ include ()
     # the headers that we need from the lastest kernel version at autogen
     # stage.
 
-    INCLUDEDIR=/lib/modules/`uname -r`/build/include/linux
+    INCLUDEDIR=${KERNEL_DIR:-/lib/modules/`uname -r`/build}/include/linux
     if [ -f $INCLUDEDIR/netfilter/nfnetlink_conntrack.h ]
     then
     	TARGET=include/libnetfilter_conntrack/linux_nfnetlink_conntrack.h
@@ -17,7 +17,7 @@ include ()
 	sed 's/linux\/netfilter\/nfnetlink.h/libnfnetlink\/linux_nfnetlink.h/g' $TARGET > $TEMP
 	mv $TEMP $TARGET
     else
-    	echo "can't find nfnetlink_conntrack.h kernel file"
+    	echo "can't find nfnetlink_conntrack.h kernel file in $INCLUDEDIR"
     	exit 1
     fi
 }

tizenorg/platform.upstream.libnetfilter_queue

autogen.sh
@@ -7,7 +7,7 @@ include ()
     # the headers that we need from the lastest kernel version at autogen
     # stage.
 
-    INCLUDEDIR=/lib/modules/`uname -r`/build/include/linux
+    INCLUDEDIR=${KERNEL_DIR:-/lib/modules/`uname -r`/build}/include/linux
     if [ -f $INCLUDEDIR/netfilter/nfnetlink_queue.h ]
     then
     	TARGET=include/libnetfilter_queue/linux_nfnetlink_queue.h
@@ -29,7 +29,7 @@ include ()
 	print $0
 	}' $TEMP > $TARGET
     else
-    	echo "can't find nfnetlink_queue.h kernel file"
+    	echo "can't find nfnetlink_queue.h kernel file in $INCLUDEDIR"
     	exit 1
     fi
 }